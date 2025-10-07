a46b507dbb5aa4890f71987038ecff9790128b3c
João <jocolognesi@gmail.com>
2025-10-07 05:13:03 -0300

    ex02

---
+++ ex02/includes/RobotomyRequestForm.hpp
@@ -0,0 +1,22 @@
+#ifndef ROBOTOMYREQUESTFORM_HPP
+#define ROBOTOMYREQUESTFORM_HPP
+
+#include "AForm.hpp"
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+
+class RobotomyRequestForm : public AForm
+{
+private:
+	std::string _target;
+public:
+	RobotomyRequestForm(const std::string& target);
+	RobotomyRequestForm(const RobotomyRequestForm& other);
+	RobotomyRequestForm& operator=(const RobotomyRequestForm& other);
+	~RobotomyRequestForm();
+	virtual void execute(const Bureaucrat& executor) const;
+};
+
+#endif
